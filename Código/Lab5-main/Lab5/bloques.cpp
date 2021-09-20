a77e5c19fd64a46327498a4c4c937f1ad43f8c75
Ferney Mejia <80012964+FerneyMP@users.noreply.github.com>
2021-09-19 23:38:07 -0500

    Add files via upload

---
+++ Código/Lab5-main/Lab5/bloques.cpp
@@ -0,0 +1,32 @@
+#include "bloques.h"
+
+bloques::bloques()
+{
+    //string l;
+    original.load(":/new/prefix1/imagenes/bloques.png");
+
+}
+
+void bloques::set_imagen(int a)
+{
+    //Proceso de recorte
+    //pixeles                     x  ,y
+    if (a==8) img = original.copy(126,14,1,1); //Se captura solo ese pedazo del recorte de bloques
+    //
+    else if(a==9) img = original.copy(0,0,1,1);
+    //
+    else img = original.copy(16*a,0,16,16);
+    //Proceso de carga de imagen
+    //Se carga la imagen y se escala
+    setPixmap(img.scaled(scalex,scaley));
+    //scaled retorna una imagen con una cantidad x,y de pixeles que se desea
+}
+
+//Modificar el tamaño de mi imagen
+void bloques::set_scale(int a, int b)
+{
+    scalex=a; //cantidad de pixeles que quiero
+    //setPixmap(img.scaled(scale,scale));
+    scaley=b;
+
+}
