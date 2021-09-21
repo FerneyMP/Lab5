84ac81c015065e8d2818fbfde0aad70cabbc174f
Ferney Mejia <80012964+FerneyMP@users.noreply.github.com>
2021-09-20 21:48:50 -0500

    Delete Código/Lab5-main/Lab5 directory

--- Código/Lab5-main/Lab5/enemigo1.cpp
+++
@@ -1,6 +0,0 @@
-#include "enemigo1.h"
-
-enemigo1::enemigo1()
-{
-    enemy1.load(":/new/prefix1/imagenes/enemigo1.png");
-}
--- Código/Lab5-main/Lab5/enemigo2.cpp
+++
@@ -1,6 +0,0 @@
-#include "enemigo2.h"
-
-enemigo2::enemigo2()
-{
-    enemy2.load(":/new/prefix1/imagenes/enemigo2.png");
-}
