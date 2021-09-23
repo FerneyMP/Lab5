20b287fa1a3dd439a77a667d9451e9a26df399a9
Ferney Mejia <80012964+FerneyMP@users.noreply.github.com>
2021-09-22 23:43:58 -0500

    Delete Lab5 directory

--- Lab5/explosion.cpp
+++
@@ -1,7 +0,0 @@
-#include "explosion.h"
-
-explosion::explosion()
-{
-    explosionHor.load(":/new/prefix1/imagenes/explosion_horizontal.png");
-    explosionVer.load(":/new/prefix1/imagenes/explosion_vertical.png");
-}
